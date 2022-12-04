aad49c199fb42f8c3754dd57f0121668c3fc081f
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-12-03 20:29:35 -0500

    Add: Album dummy CRUD

---
+++ src/data/albums.js
@@ -0,0 +1,141 @@
+export default [
+  {
+    id: 1,
+    albumTitle: 'Blurryface',
+    albumAuthor: 'Twenty One Pilots',
+    albumSlug: 'blurryface',
+    albumCover:
+      'https://upload.wikimedia.org/wikipedia/en/7/7d/Blurryface_by_Twenty_One_Pilots.png',
+    albumSongs: [
+      {
+        id: 1,
+        songTitle: 'Heavydirtysoul',
+        songSlug: 'heavydirtysoul',
+        songLength: '3:38',
+      },
+      {
+        id: 2,
+        songTitle: 'Stressed Out',
+        songSlug: 'stressed-out',
+        songLength: '3:23',
+      },
+      {
+        id: 3,
+        songTitle: 'Ride',
+        songSlug: 'ride',
+        songLength: '3:03',
+      },
+      {
+        id: 4,
+        songTitle: 'Fairly Local',
+        songSlug: 'fairly-local',
+        songLength: '3:15',
+      },
+    ],
+    albumReleaseDate: '2015-05-15',
+  },
+  {
+    id: 2,
+    albumTitle: 'Trench',
+    albumAuthor: 'Twenty One Pilots',
+    albumSlug: 'trench',
+    albumCover:
+      'https://upload.wikimedia.org/wikipedia/en/f/f3/Trench_Twenty_One_Pilots.png',
+    albumSongs: [
+      {
+        id: 1,
+        songTitle: 'Jumpsuit',
+        songSlug: 'jumpsuit',
+        songLength: '3:38',
+      },
+      {
+        id: 2,
+        songTitle: 'Levitate',
+        songSlug: 'levitate',
+        songLength: '3:23',
+      },
+      {
+        id: 3,
+        songTitle: 'Nico and the Niners',
+        songSlug: 'nico-and-the-niners',
+        songLength: '3:03',
+      },
+    ],
+    albumReleaseDate: '2018-10-05',
+  },
+  {
+    id: 3,
+    albumTitle: 'Vessel',
+    albumAuthor: 'Twenty One Pilots',
+    albumSlug: 'vessel',
+    albumCover:
+      'https://upload.wikimedia.org/wikipedia/en/2/20/Vessel_by_Twenty_One_Pilots.jpg',
+    albumSongs: [
+      {
+        id: 1,
+        songTitle: 'Car Radio',
+        songSlug: 'car-radio',
+        songLength: '3:38',
+      },
+      {
+        id: 2,
+        songTitle: 'Holding On To You',
+        songSlug: 'holding-on-to-you',
+        songLength: '3:23',
+      },
+      {
+        id: 3,
+        songTitle: 'Trees',
+        songSlug: 'trees',
+        songLength: '3:03',
+      },
+    ],
+    albumReleaseDate: '2022-10-21',
+  },
+  {
+    id: 4,
+    albumTitle: 'Scaled and Icy',
+    albumAuthor: 'Twenty One Pilots',
+    albumSlug: 'scaled-and-icy',
+    albumCover:
+      'https://upload.wikimedia.org/wikipedia/en/5/52/Twenty_One_Pilots_-_Scaled_and_Icy.png',
+    albumSongs: [
+      {
+        id: 1,
+        songTitle: 'Shy Away',
+        songSlug: 'shy-away',
+        songLength: '3:38',
+      },
+      {
+        id: 2,
+        songTitle: 'Choker',
+        songSlug: 'choker',
+        songLength: '3:23',
+      },
+    ],
+    albumReleaseDate: '2021-05-21',
+  },
+  {
+    id: 5,
+    albumTitle: 'Midnights',
+    albumAuthor: 'Taylor Swift',
+    albumSlug: 'midnights',
+    albumCover:
+      'https://upload.wikimedia.org/wikipedia/en/9/9f/Midnights_-_Taylor_Swift.png',
+    albumSongs: [
+      {
+        id: 1,
+        songTitle: 'Maroon',
+        songSlug: 'maroon',
+        songLength: '3:38',
+      },
+      {
+        id: 2,
+        songTitle: 'Snow at the beach',
+        songSlug: 'snow-at-the-beach',
+        songLength: '3:23',
+      },
+    ],
+    albumReleaseDate: '2021-05-21',
+  },
+];
--- src/server.js
+++ src/server.js
@@ -1,11 +1,78 @@
 import express from 'express';
+import albums from './data/albums.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.get('/api/v1/albums', (req, res) => {
+  res.status(200).json(albums);
+});
+
+app.get('/api/v1/albums/:slug', (req, res) => {
+  const album = albums.find((album) => album.albumSlug === req.params.slug);
+
+  if (!album) {
+    return res.status(404).send('Album not found');
+  }
+
+  res.status(200).json(album);
+});
+
+app.post('/api/v1/albums', (req, res) => {
+  const { albumTitle, albumReleaseDate, albumCover, albumAuthor } = req.body;
+
+  const newAlbum = {
+    id: albums.length + 1,
+    albumTitle,
+    albumSlug: albumTitle.toLowerCase().replace(/ /g, '-'),
+    albumReleaseDate,
+    albumCover,
+    albumAuthor,
+  };
+
+  albums.push(newAlbum);
+
+  res.status(201).json(newAlbum);
+});
+
+app.patch('/api/v1/albums/:slug', (req, res) => {
+  const album = albums.find((album) => album.albumSlug === req.params.slug);
+
+  if (!album) {
+    return res.status(404).send('Album not found');
+  }
+
+  const { albumTitle, albumReleaseDate, albumCover, albumAuthor } = req.body;
+
+  album.albumTitle = albumTitle;
+  album.albumSlug = albumTitle.toLowerCase().replace(/ /g, '-');
+  album.albumReleaseDate = albumReleaseDate;
+  album.albumCover = albumCover;
+  album.albumAuthor = albumAuthor;
+
+  res.status(200).json(album);
+});
+
+app.delete('/api/v1/albums/:slug', (req, res) => {
+  const album = albums.find((album) => album.albumSlug === req.params.slug);
+
+  if (!album) {
+    return res.status(404).send('Album not found');
+  }
+
+  const index = albums.indexOf(album);
+
+  albums.splice(index, 1);
+
+  res.status(200).json({ message: 'Album deleted' });
+});
+
+app.listen(2000, () => {
+  console.log('Server listening on port 2000');
 });
